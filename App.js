b7916c2b24041ad39bdc05980f2ab5fb20a43a92
shakyasaijal <saijalshakya@gmail.com>
2020-04-10 14:39:05 +0545

    init: dev

--- App.js
+++ App.js
@@ -3,17 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>One House Solutions</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
